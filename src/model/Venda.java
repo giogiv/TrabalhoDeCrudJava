ca0c3500d84af01f98f33888f3655efeb1f1d97b
Kimberly Rolim <20182PF.CC0076@passofundo.ifsul.local>
2025-09-02 14:37:30 -0300

    TrabalhoJava

---
+++ src/model/Venda.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 20182PF.CC0076
+ */
+import java.time.LocalDateTime;
+
+public class Venda {
+    private LocalDateTime dataVenda;
+    private double valorVenda;
+    private FormaPgto formaPgto;
+    private FormaContrato formaContrato;
+
+    public Venda(LocalDateTime dataVenda, double valorVenda, FormaPgto formaPgto, FormaContrato formaContrato) {
+        this.dataVenda = dataVenda;
+        this.valorVenda = valorVenda;
+        this.formaPgto = formaPgto;
+        this.formaContrato = formaContrato;
+    }
+
+    public LocalDateTime getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDateTime dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public FormaPgto getFormaPgto() {
+        return formaPgto;
+    }
+
+    public void setFormaPgto(FormaPgto formaPgto) {
+        this.formaPgto = formaPgto;
+    }
+
+    public FormaContrato getFormaContrato() {
+        return formaContrato;
+    }
+
+    public void setFormaContrato(FormaContrato formaContrato) {
+        this.formaContrato = formaContrato;
+    }
+}
+
